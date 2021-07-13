5ad96a91ed4b58c61e5e647c6d27ddba9f26cc42
GoddessJackass <bozicajakas@gmail.com>
2021-07-13 21:24:37 +0200

    Update to app.js
    - var to const;
    - append with document.body
    - use setAttribute
    - remove comments

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
-// https://docs.github.com/en/rest
-
 const endpoint = "https://api.github.com/users/bjakas";
-
-// SKRAĆENA VERZIJA 
 
 fetch(endpoint) // 1. promise
   .then((response) => {
     return response.json(); // 2. promise
   })
   .then((data) => {
-    var body = document.querySelector("body");
-    var h1 = document.createElement("h1");
+    const h1 = document.createElement("h1");
     h1.innerText = data.login;
-    body.appendChild(h1);
-    var a = document.createElement("a");
-    a.innerText = "Visit my GitHub page";
-    body.appendChild(a);
-    // adding link
-    a.href = data.html_url;
-    var img = document.createElement("img");
-    body.appendChild(img);
-    img.src = "https://avatars.githubusercontent.com/u/74099367?v=4";
-    img.alt = `${data.login} profile image`;
+    document.body.appendChild(h1);
 
+    const a = document.createElement("a");
+    a.setAttribute("href", data.html_url);
+    // OR: a.href = data.html_url;
+    a.innerText = "Visit my GitHub page";
+    document.body.appendChild(a);
+
+
+    const img = document.createElement("img");
+    img.setAttribute("src", data.avatar_url); // OR: img.src = data.avatar_url;
+    img.setAttribute("alt", `${data.login} profile image`); // OR: img.alt = `${data.login} profile image`;
+    document.body.appendChild(img);
 
   })
   .catch((error) => {
     console.error(error);
   });
 
-//  console.log(data.login);  u consoli: bjakas https://github.com/bjakas
-// console.log(data.html_url);
-
-/* DUŽA VERZIJA
-fetch(endpoint)   // šaljemo zahtjev na naš endpoint // 1. promise
-.then((response) => {
-  response.json() // 2. promise
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error); // console.error je još jedna metoda za ispisivanje tekstau  consoli, tekst bude crven
-    });
-})
-.catch((error) => {
-  console.log(error);
-})
-*/
